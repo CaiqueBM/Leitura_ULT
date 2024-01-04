32092e890beb3f6742334cd5aac48915db0c6c03
Caique <caiquebmaso@ucl.br>
2024-01-04 14:13:50 -0300

    14

--- op_diaria/envio.py
+++ op_diaria/envio.py
@@ -29,13 +29,14 @@ def enviar_mensagem(df):
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
     options.add_argument("--enable-logging --v=1")
+    options.add_argument("--no-first-run")
+    options.add_argument("--no-default-browser-check")
     options.add_argument("--user-data-dir=/home/abs/.config/google-chrome/")
     # provide the profile name with which we want to open browser
     options.add_argument("--profile-directory=Profile 1")
+
     # options.add_argument("--profile-directory=Profile 1")  # Path to your chrome profile
     driver = webdriver.Chrome(options=options)
-
-    # options = webdriver.ChromeOptions()
 
     # options.add_argument("--headless")
     # driver = webdriver.Chrome(options=options)
