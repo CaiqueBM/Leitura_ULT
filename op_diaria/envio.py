6452de3f8d275a396f3e789a5b2713a6f661544f
Caique <caiquebmaso@ucl.br>
2024-01-04 10:16:21 -0300

    Atualizaçao 1.0

--- op_diaria/envio.py
+++ op_diaria/envio.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pywhatkit
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -7,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
-from urllib.parse import quote
 
 
 # --------------------- MENSAGEM WHATSAPP ------------------------
